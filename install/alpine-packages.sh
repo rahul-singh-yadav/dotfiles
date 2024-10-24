77e39869edab2959b02863557a8dd6a31d79dfbb
rahul-singh-yadav <iam.rahulyadav@outlook.com>
2024-10-24 20:31:08 +0530

    added double quotes to prevent word-splitting

--- install/alpine-packages.sh
+++ install/alpine-packages.sh
@@ -36,10 +36,9 @@ echo "System packages installed successfully!"
 # Zsh is installed, now set Zsh as the default shell for the current user
 if [ -x "$(command -v zsh)" ]; then
     echo "Setting Zsh as the default shell..."
-    chsh -s $(which zsh) || echo "chsh command not available, set Zsh manually."
+    chsh -s "$(which zsh)" || echo "chsh command not available, set Zsh manually."
 else
     echo "Zsh installation failed. Please check the logs."
 fi
 
 echo "Alpine packages installation complete!"
-
